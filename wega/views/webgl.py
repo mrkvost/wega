79f3e2216d1e68c4c201ccb8282023cbaea30e74
Michal Hoľa <mrkvost@gmail.com>
2015-01-10 18:17:20 -0500

    added x3dom page

--- wega/views/webgl.py
+++ wega/views/webgl.py
@@ -4,6 +4,11 @@ from flask import Blueprint, render_template, url_for
 webgl_app = Blueprint('webgl', __name__)
 
 
-@webgl_app.route('/')
-def index():
-    return render_template('webgl.html')
+@webgl_app.route('/threejs')
+def threejs():
+    return render_template('webgl-threejs.html')
+
+
+@webgl_app.route('/x3dom')
+def x3dom():
+    return render_template('webgl-x3dom.html')
