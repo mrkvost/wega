1b1a2a04fe8b9dc22a6014833c2aa6e1d2b267ee
Michal Hoľa <mrkvost@gmail.com>
2015-01-27 18:17:20 -0500

    basic order layout

--- wega/views/order.py
+++ wega/views/order.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, render_template
 
+from wega.db.core import db
+from wega.db.models import Order, User
+
+
+IMAGE_PLACEHOLDER_SMALL = 'holder.js/200x300/#333:#fff/text:missing image'
+IMAGE_PATH = 'img/products'
+
 
 order_app = Blueprint('order', __name__)
 
 
 @order_app.route('')
 def index():
-    return render_template('order.html')
+    order = Order.query.join(User, aliased=True).\
+        filter_by(username='admin').first()
+    total = sum([
+        item.product.price * item.number for item in order.items
+    ])
+    return render_template('order.html', order=order, total=total)
